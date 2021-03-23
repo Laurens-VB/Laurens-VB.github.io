e0cd4655e8c83dd8d75c9111f11a7f7eb3d5fcfc
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-03-23 10:54:39 +0100

    Update box.js

--- box/box.js
+++ box/box.js
@@ -85,7 +85,7 @@
 			}
 			
 			if(val == 50){
-				console.log("HeeHeeHaaHaaJooJoooLmfao")
+				this.$style.innerHTML = ':host {border-radius: 10px;border-width: 2px;border-color: black;border-style: solid;display: block;}.body {background: #fff;}.metric {padding: 10%;}.metric svg {max-width: 100%;}.metric path {stroke-width: 75;stroke: #ecf0f1;fill: none;}.metric text {font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;}.metric.participation path.data-arc {stroke: ' + '#db821d' + ';}.metric.participation text {fill: ' + '#db821d' + ';}';
 			}
 		}
 		  
