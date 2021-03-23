3850d7979f72f311011e304a490abf84879577db
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-03-23 17:10:53 +0100

    Hehhhhn

--- box/box.js
+++ box/box.js
@@ -84,6 +84,8 @@
 				this.$svg.innerHTML = '<path d="M 950 500 A 450 450 0 0 0 50 500"></path><text class="percentage" text-anchor="middle" alignment-baseline="middle" x="500" y="300" font-size="140" font-weight="bold">' + rounded + '%</text><text class="title" text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal">' + info + '</text><path d="' + x + '" class="data-arc"></path>"';
 			}
 
+			console.log(treshholds);
+
 			/*
 
 			if(treshholds != undefined)
