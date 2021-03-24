0f5af905a7dd01b5a07b0df7e3a59ba4547fc913
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-03-24 14:01:20 +0100

    Update box.js

--- box/box.js
+++ box/box.js
@@ -92,8 +92,15 @@
 				console.log(decodedCookie);
 				var split_decodedCookie = decodedCookie.split(",");
 				console.log("FOR LOOP");
-				for(var slicedCookie in split_decodedCookie){
-					console.log(slicedCookie);
+				for(var i in split_decodedCookie){
+					var threshhold = split_decodedCookie[i].split("->");;
+
+					var waarde = threshhold[0];
+					var kleur = threshhold[1];
+
+					if(val >= waarde){
+						this.$style.innerHTML = ':host {border-radius: 10px;border-width: 2px;border-color: black;border-style: solid;display: block;}.body {background: #fff;}.metric {padding: 10%;}.metric svg {max-width: 100%;}.metric path {stroke-width: 75;stroke: #ecf0f1;fill: none;}.metric text {font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;}.metric.participation path.data-arc {stroke: ' + kleur + ';}.metric.participation text {fill: ' + kleur + ';}';
+					}
 				}
 				console.log("-----------------------------------");
 			}
@@ -103,7 +110,7 @@
 				var threshholds_split = treshholds.slice(0,-1).split("|");
 				for(var threshholdIndex in threshholds_split)
 				{
-					var threshhold = (threshholds_split[threshholdIndex]).split("->");
+					var threshhold = (threshholds_split[threshholdIndex])
 					
 					var waarde = threshhold[0];
 					var kleur = threshhold[1];
