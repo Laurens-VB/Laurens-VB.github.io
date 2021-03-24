1ec80ad4076f33b3e1037f934ae4caa9f9a31696
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-03-24 22:53:50 +0100

    Update colorpicker.js

--- colorpicker/colorpicker.js
+++ colorpicker/colorpicker.js
@@ -4,7 +4,7 @@
         <h1>LAURENS HAIP</h1>
     `;
 
-    customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+    customElements.define('com-colorpicker', class HelloWorld1 extends HTMLElement {
 
 
 		constructor() {
