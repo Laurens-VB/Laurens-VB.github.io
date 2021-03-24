dbe7434faffe295ab7b1214700df90228eb04f0f
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-03-24 15:01:31 +0100

    Update box.js

--- box/box.js
+++ box/box.js
@@ -101,10 +101,6 @@
 					decodedCookie = decodedCookie + addToCookie;
 
 					console.log("<><><><><><>");
-					
-					if(opslaan){
-						document.cookie = "SavedThreshhold="+ decodedCookie;
-					}
 				}
 				console.log(decodedCookie);
 				var split_decoded_cookie = decodedCookie.split(",");
