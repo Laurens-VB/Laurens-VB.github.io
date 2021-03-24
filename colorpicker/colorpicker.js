fce288fdab5d7a6a102184aa9ebaaa6b1624a585
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-03-24 22:50:07 +0100

    ColoerPicker

--- colorpicker/colorpicker.js
+++ colorpicker/colorpicker.js
@@ -1,57 +1,47 @@
-(function()  {
-    let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
+(function()
+{
+    let colorpickerHTML = document.createElement("colorpickerHTML");
+	colorpickerHTML.innerHTML = `
         <h1>Hello World</h1>
-    `;
+	`;
 
-    customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+    class Colorpicker extends HTMLElement 
+    {
+        constructor ()
+        {
+            super();
+            let shadowRoot = this.attachShadow(
+            {
+                mode: "open"
+            });
 
+            shadowRoot.appendChild(colorpickerHTML.content.cloneNode(true));
 
-		constructor() {
-			super(); 
-			this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._firstConnection = false;
-		}
+            this.addEventListener("click", event => {
+                var event = new Event("onClick");
+                this.dispatchEvent(event);
+            })
 
-        //Fired when the widget is added to the html DOM of the page
-        connectedCallback(){
-            this._firstConnection = true;
-            this.redraw();
+            this._props = {};
+
         }
 
-         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-        disconnectedCallback(){
-        
+        render(color)
+        {
+            colorChoice = colorpickerHTML.querySelector("#color");
+
+            colorChoice.addEventListener("keuzeKleur", () =>{
+                let kleur = colorChoice.value;
+                console.log("kleur" = kleur);
+                console.log("color" = color)
+
+            })
         }
 
-         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-		onCustomWidgetBeforeUpdate(oChangedProperties) {
-
-		}
-
-        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-		onCustomWidgetAfterUpdate(oChangedProperties) {
-            if (this._firstConnection){
-                this.redraw();
-            }
+        onCustomWidgetAfterUpdate(changedProperties){
+            this.render();
         }
-        
-        //When the custom widget is removed from the canvas or the analytic application is closed
-        onCustomWidgetDestroy(){
-        }
+    }
 
-        
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
-        /*
-        onCustomWidgetResize(width, height){
-            redraw()
-        }
-        */
-
-        redraw(){
-        }
-    });
+    customElements.define("com-colorpicker", Colorpicker);
 })();
