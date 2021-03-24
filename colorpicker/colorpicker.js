8263fa51cd6dfdfe10b695e4b58b49747002ec51
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-03-24 23:04:42 +0100

    Update colorpicker.js

--- colorpicker/colorpicker.js
+++ colorpicker/colorpicker.js
@@ -1,7 +1,7 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <h1>LVB</h1>
+        <h1>Hello World</h1>
     `;
 
     customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
@@ -11,10 +11,13 @@
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
 		}
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
+            this._firstConnection = true;
+            this.redraw();
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -29,7 +32,9 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
-            this.redraw();
+            if (this._firstConnection){
+                this.redraw();
+            }
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
@@ -38,12 +43,15 @@
 
         
         //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default
+        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
         /*
         onCustomWidgetResize(width, height){
+            redraw()
         }
         */
 
-        redraw(){}
+        redraw(){
+        }
     });
 })();
