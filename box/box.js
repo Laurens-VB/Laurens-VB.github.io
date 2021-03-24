9eb7bc63b4b46fa75903ceebeb6e91f537e1dbb6
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-03-24 15:05:10 +0100

    Opslaan fixen

--- box/box.js
+++ box/box.js
@@ -114,6 +114,11 @@
 						this.$style.innerHTML = ':host {border-radius: 10px;border-width: 2px;border-color: black;border-style: solid;display: block;}.body {background: #fff;}.metric {padding: 10%;}.metric svg {max-width: 100%;}.metric path {stroke-width: 75;stroke: #ecf0f1;fill: none;}.metric text {font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;}.metric.participation path.data-arc {stroke: ' + kleur + ';}.metric.participation text {fill: ' + kleur + ';}';
 					}
 				}
+
+				if(opslaan){
+					document.cookie = "SavedThreshhold=" + split_decoded_cookie;
+					console.log("aanpassing opgeslagen");
+				}
 			}
 
 			if(treshholds != undefined)
