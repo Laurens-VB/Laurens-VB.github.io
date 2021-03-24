b178eedd4d3f71c287f317aa1787abc7f714f3ee
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-03-24 23:52:33 +0100

    typo

--- colorpicker/colorpicker.js
+++ colorpicker/colorpicker.js
@@ -1,6 +1,6 @@
 (function()  {
 
-    let inputField = tmpl.createElement('input');
+    let inputField = document.createElement('input');
     inputField.setAttribute('type', 'color');
     inputField.setAttribute('id', 'color');
 
@@ -9,7 +9,7 @@
 		constructor() {
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._shadowRoot.appendChild(inputField.content.cloneNode(true));
             this._firstConnection = false;
 		}
         
@@ -37,9 +37,6 @@
         }
 
         redraw(){
-            let colorInputField = tmpl.querySelector('#color');
-
-            console.log(colorInputField);
             console.log("lmfao");
 
 
