6dfccb8bc4b0a56ce48f730d47df743c17f2ac35
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-03-25 00:58:59 +0100

    Update colorpicker.js

--- colorpicker/colorpicker.js
+++ colorpicker/colorpicker.js
@@ -1,1 +1,99 @@
-console.log("banana");
+(function()  {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+        <input type="color" id="color">
+    `;
+
+    customElements.define('com-colorpicker', class ColorPicker extends HTMLElement 
+    {
+
+		constructor() {
+			super(); 
+			this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
+
+            
+            this.$colorInputField = this._shadowRoot.querySelector('#color');
+            
+            this.addEventListener("input", () => {
+                console.log("<><><><><><>");
+                console.log(this.$colorInputField.value);
+                console.log("<><><><><><>");
+			});
+		}
+        
+
+        connectedCallback(){
+            this._firstConnection = true;
+            this.redraw();
+        }
+
+        disconnectedCallback(){
+        
+        }
+
+		onCustomWidgetBeforeUpdate(oChangedProperties) {
+
+		}
+
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+            if (this._firstConnection){
+                this.redraw();
+            }
+
+
+            /*
+
+            var color = "";
+
+
+
+            console.log(color);
+
+            oChangedProperties["color"] = color;
+
+            if ("color" in oChangedProperties) {
+				this.$color = oChangedProperties["color"];
+			}
+
+            */
+        }
+        
+        onCustomWidgetDestroy(){
+        }
+
+        redraw(){
+
+
+           /*
+            var color;
+            console.log("so far so goodly");
+            tmpl.addEventListener('input', () =>
+            {
+                console.log("kekw")
+                color = tmpl.nodeValue;
+            })
+
+            console.log(color);
+            console.log("lmfao");
+            */
+
+
+
+            /*
+            var colorChoice;
+
+            colorInputField.addEventListener('input', () =>{
+                colorChoice = colorInputField.nodeValue;
+            })
+
+            if(colorChoice != undefined){
+                console.log(colorChoice);
+                console.log("kekw");
+            }
+            console.log("lmfao");
+            */
+        }
+    });
+})();
