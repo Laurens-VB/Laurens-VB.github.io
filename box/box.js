7ca79b211ddd3eb01793446be534de3143b527aa
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-04-12 01:46:02 +0200

    Test

--- box/box.js
+++ box/box.js
@@ -38,10 +38,16 @@
 		
 		.metric.participation text {
 		  fill: #27ae60;
-		}		
+		}
+		
+		h1{
+			text-align: center;
+		}
+
 		</style>
 		
 		<div class="container">
+		<h1></h1>
 		  <div class="row">
 		    <div class="col-md-4 col-sm-4">
 		      <div class="metric participation" data-ratio=".95">
@@ -100,6 +106,7 @@
 				if(rounded >=0 && rounded <=100) {
 					this.$style.innerHTML = ':host {border-radius: 10px;border-width: 2px;border-color: black;border-style: solid;display: block;}.body {background: #fff;}.metric {padding: 10%;}.metric svg {max-width: 100%;}.metric path {stroke-width: 75;stroke: #ecf0f1;fill: none;}.metric text {font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;}.metric.participation path.data-arc {stroke: ' + color + ';}.metric.participation text {fill: ' + color + ';}';
 					this.$svg.innerHTML = '<path d="M 950 500 A 450 450 0 0 0 50 500"></path><text text-anchor="middle" alignment-baseline="middle" x="500" y="300" font-size="140" font-weight="bold">' + measureValue + '</text><text class="title" text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal">' + "/" + compareTo +  '</text><path d="' + x + '" class="data-arc"></path>"';
+					this.$h1.innerHTML = 'TEST';
 				}
 			}
 			else{
@@ -121,6 +128,7 @@
 					+ '</text><path d="' 
 					+ x 
 					+ '" class="data-arc"></path>"';
+					this.$h1.innerHTML = 'TEST';
 				}
 			}
 
