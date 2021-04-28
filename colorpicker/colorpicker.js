02cdedbde5c4ee58f81a1582e450a260a27cb1c2
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-04-28 14:58:49 +0200

    plz plz plz werk

--- colorpicker/colorpicker.js
+++ colorpicker/colorpicker.js
@@ -13,7 +13,6 @@
     <h1>WORLDMAP 3</h1>
     <div id="map" style="width: 600px; height: 400px; top: 50px;">
         <p><a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a></p>
-        <script src="leafletMap/initiateLeaflet.js"> </script>
     </div>
     </body>
     `;
@@ -23,6 +22,12 @@
     headScript.src = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.js";
     document.head.appendChild(headScript);
     console.log("HEEE HEEE");
+
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://laurens-vb.github.io/leafletMap/initiateLeaflet.js";
+    document.getElementById("map").appendChild(script);
+    console.log("HEEE HEEE 2");
 
     customElements.define('com-colorpicker', ColorPicker)
     
