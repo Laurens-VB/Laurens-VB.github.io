e75ac22a87d8f43e637f16bb201a7793599f6327
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-04-28 15:08:03 +0200

    aaaa

--- colorpicker/colorpicker.js
+++ colorpicker/colorpicker.js
@@ -1,57 +1,49 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.js"></script>
-        <style>
-          #map {position: absolute; top: 0; right: 0; bottom: 0; left: 0;}
-        </style>
-      </head>
-      <body>
-        <div id="map"></div>
+    <head>
+    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css" />
+    <style>
+        #map {position: absolute; top: 0; right: 0; bottom: 0; left: 0;}
+    </style>
+    </head>
+    <body>
+
+    <h1>WORLDMAP 3</h1>
+    <div id="map" style="width: 600px; height: 400px; top: 50px;">
         <p><a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a></p>
-        <script>
-          var map = L.map('map').setView([50.641111, 4.668056], 1);
-          L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=tLle2wcpHPrfuS2ObIb7',{
-            tileSize: 512,
-            zoomOffset: -1,
-            minZoom: 5,
-            attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-            crossOrigin: true
-          }).addTo(map);
-               
-            addMarkerToMap(50.641111,4.668056,"BE",map);
-            addMarkerToMap(51,10,"DE",map);
-            addMarkerToMap(47,2,"FR",map);
-    
-          function addMarkerToMap(lat,lng,name,map)
-            {
-                L.marker([lat,lng]).addTo(map).on('click', () =>
-                {
-                    console.log(name);
-                });
-            }
-    
-        </script>
-      </body>
-    </html>
+    </div>
+    </body>
     `;
 
-    customElements.define('com-leafletMap', class LeafletMap extends HTMLElement 
+    var headScript = document.createElement("script");
+    headScript.type = "text/javascript";
+    headScript.src = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.js";
+    document.head.appendChild(headScript);
+    console.log("HEEE HEEE");
+
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "initiateLeaflet.js";
+    document.head.appendChild(script);
+    console.log("HEEE HEEE 2");
+
+    customElements.define('com-colorpicker', ColorPicker)
+    
+    class ColorPicker extends HTMLElement 
     {
 
 		constructor() {
+            
+
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
 
-            
-            /*this.$colorInputField = this._shadowRoot.querySelector('#color');
+            /*
+            this.$colorInputField = this._shadowRoot.querySelector('#color');
 
             this.addEventListener("input", () => {
                 var properties = {color : this.$colorInputField.value };
@@ -64,6 +56,8 @@
                 }));
                 
 			});*/
+
+            console.log("ZAWARDO");
 		}
         
 
@@ -91,5 +85,5 @@
 
         redraw(){
         }
-    });
+    };
 })();
