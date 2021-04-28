024bd34e78e5e073a706df4186fa4bfc9c5fda0e
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-04-28 16:33:15 +0200

    Update colorpicker.js

--- colorpicker/colorpicker.js
+++ colorpicker/colorpicker.js
@@ -1,47 +1,19 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-    <head>
-    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css" />
-    <style>
-        #map {position: absolute; top: 0; right: 0; bottom: 0; left: 0;}
-    </style>
-    </head>
-    <body>
-
-    <h1>WORLDMAP 3</h1>
-    <div id="map" style="width: 600px; height: 400px; top: 50px;">
-        <p><a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a></p>
-    </div>
-    </body>
+        <embed type="text/html" src="https://laurens-vb.github.io/leafletTest.html"  width="500" height="200">
     `;
 
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "https://laurens-vb.github.io/colorpicker/initiateLeaflet.js";
-    document.body.appendChild(script);
-
-    var headScript = document.createElement("script");
-    headScript.type = "text/javascript";
-    headScript.src = "https://laurens-vb.github.io/leaflet/leaflet.js";
-    document.head.appendChild(headScript);
-    console.log("leaflet loaded");
-
-
-    
     customElements.define('com-colorpicker', class ColorPicker extends HTMLElement 
     {
 
 		constructor() {
-            
-
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
 
-            /*
+            
             this.$colorInputField = this._shadowRoot.querySelector('#color');
 
             this.addEventListener("input", () => {
@@ -54,9 +26,7 @@
                     }
                 }));
                 
-			});*/
-
-            console.log("ZAWARDO");
+			});
 		}
         
 
