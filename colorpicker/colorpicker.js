ba19c424f1962cc3a9ce156fee396de4c64ab340
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-04-28 19:12:02 +0200

    Update colorpicker.js

--- colorpicker/colorpicker.js
+++ colorpicker/colorpicker.js
@@ -4,11 +4,6 @@
         <embed id = "embeddedHtml" type="text/html" src="https://laurens-vb.github.io/leafletTest.html"  width="700" height="700">
     `;
 
-    console.log("----------");
-    console.log(document.getElementById("embeddedHtml"));
-    console.log("----------");
-
-
     customElements.define('com-colorpicker', class ColorPicker extends HTMLElement 
     {
 
@@ -18,76 +13,29 @@
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
 
-            /*
-            this.addEventListener("onClicked", () => {
-                /*var properties = {color : this.$colorInputField.value };
-                this.dispatchEvent(new CustomEvent("propertiesChanged", 
-                {
-                    detail: 
-                    {
-                        properties: properties
-                    }
-                }));
-                console.log("HEEE HEEE HEEE HAAA HAAA HAAA HOO HOOO HOOO");
-                console.log("REEEEEEE");
-			})*/
+            
+            this.addEventListener("onstorage", () => {
+                console.log("plz plz plz do something :'(");
+			})
 		}
         
 
         connectedCallback(){
             this._firstConnection = true;
             this.redraw();
-
-            var cookieObj = document.cookie
-                .split(';')
-                .map(cookie => cookie.split('='))
-                .reduce((accumulator, [key,value]) =>
-                    ({...accumulator, [key.trim()]: decodeURIComponent(value) }),
-                {});
-        
-            console.log("callbackConnected")
-            console.log(cookieObj.selectedLocation);
         }
 
         disconnectedCallback(){
-            var cookieObj = document.cookie
-                .split(';')
-                .map(cookie => cookie.split('='))
-                .reduce((accumulator, [key,value]) =>
-                    ({...accumulator, [key.trim()]: decodeURIComponent(value) }),
-                {});
-        
-            console.log("disconnectedCallback")
-            console.log(cookieObj.selectedLocation);
-        
         }
 
 		onCustomWidgetBeforeUpdate(oChangedProperties) {
-            var cookieObj = document.cookie
-                .split(';')
-                .map(cookie => cookie.split('='))
-                .reduce((accumulator, [key,value]) =>
-                    ({...accumulator, [key.trim()]: decodeURIComponent(value) }),
-                {});
-        
-            console.log("beforeUpdate")
-            console.log(cookieObj.selectedLocation);
+
 		}
 
 		onCustomWidgetAfterUpdate(oChangedProperties) {
             if (this._firstConnection){
                 this.redraw();
             }
-
-            var cookieObj = document.cookie
-                .split(';')
-                .map(cookie => cookie.split('='))
-                .reduce((accumulator, [key,value]) =>
-                    ({...accumulator, [key.trim()]: decodeURIComponent(value) }),
-                {});
-        
-            console.log("afterUpdate")
-            console.log(cookieObj.selectedLocation);
         }
         
         onCustomWidgetDestroy(){
