8cc15a35cc82fdc5c33351794b99455802edee73
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-04-28 14:01:41 +0200

    Update colorpicker.js

--- colorpicker/colorpicker.js
+++ colorpicker/colorpicker.js
@@ -25,20 +25,6 @@
         {
             console.log("me neme is jef");
   
-            var map = L.map('map').setView([50.641111, 4.668056], 1);
-            L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=tLle2wcpHPrfuS2ObIb7',{
-            tileSize: 512,
-            zoomOffset: -1,
-            minZoom: 0,
-            attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-            crossOrigin: true
-            }).addTo(map);
-             
-            addMarkerToMap(50.641111,4.668056,"BE",map);
-            addMarkerToMap(51,10,"DE",map);
-            addMarkerToMap(47,2,"FR",map);
-
-
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
@@ -87,13 +73,5 @@
 
         redraw(){
         }
-
-        addMarkerToMap(lat,lng,name,map)
-          {
-              L.marker([lat,lng]).addTo(map).on('click', () =>
-              {
-                  console.log(name);
-              });
-          }
     });
 })();
