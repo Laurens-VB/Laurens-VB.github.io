2ddb70400e48163ee3ff008abe16cef6b14f5bc2
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-04-28 14:16:51 +0200

    move init

---
+++ leafletMap/initiateLeaflet.js
@@ -0,0 +1,22 @@
+console.log("mjef me name");
+
+var map = L.map('map').setView([50.641111, 4.668056], 1);
+L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=tLle2wcpHPrfuS2ObIb7',{
+  tileSize: 512,
+  zoomOffset: -1,
+  minZoom: 0,
+  attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+  crossOrigin: true
+}).addTo(map);
+     
+  addMarkerToMap(50.641111,4.668056,"BE",map);
+  addMarkerToMap(51,10,"DE",map);
+  addMarkerToMap(47,2,"FR",map);
+
+function addMarkerToMap(lat,lng,name,map)
+  {
+      L.marker([lat,lng]).addTo(map).on('click', () =>
+      {
+          console.log(name);
+      });
+  }
