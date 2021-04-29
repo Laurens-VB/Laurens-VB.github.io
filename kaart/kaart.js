6e59e24bb4c5452405b981662b760deea83a3ee0
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-04-29 15:57:08 +0200

    Update kaart.js

--- kaart/kaart.js
+++ kaart/kaart.js
@@ -1,20 +1,8 @@
 (function()  {
-    /*let tmpl = document.createElement('template');
+    let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-    <h1 id="H1">BRO</h1>
-    <head>
-    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.js"></script>
-    <style>
-        #map {position: absolute; top: 50; right: 0; bottom: 0; left: 0;}
-    </style>
-    </head>
-    <body>
-    <div id="map">
-        <p><a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a></p>
-    </div>
-    `;*/
+    <button id="buttonClick">Click Me</button>
+    `;
 
     var script = document.createElement('script');
     script.innerHTML = `console.log("TEST")`;
@@ -23,24 +11,26 @@
     customElements.define('com-geomap', class geomap extends HTMLElement 
     {
 
-		constructor() {
+		constructor() 
+        {
             super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(script.content.cloneNode(true));
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
 
             
-            this.$embeddedHtml = this._shadowRoot.querySelector('#embeddedHtml');
+            this.$embeddedHtml = this._shadowRoot.querySelector('#buttonClick');
 
-            this.addEventListener("mouseover", () =>{
-                var properties = {selectedRegionISO2 : document.cookie};
+            this.addEventListener("click", () =>{
+                console.log("JEF");
+                /*var properties = {selectedRegionISO2 : document.cookie};
                 this.dispatchEvent(new CustomEvent("propertiesChanged", 
                 {
                     detail: 
                     {
                         properties: properties
                     }
-                }))
+                }))*/
             });
 
             /*
