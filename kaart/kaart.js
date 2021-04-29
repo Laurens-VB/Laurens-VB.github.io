b2dde43e85557b2a29c9b826716a122ee661fe93
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-04-29 18:03:26 +0200

    Update kaart.js

--- kaart/kaart.js
+++ kaart/kaart.js
@@ -42,14 +42,14 @@
 
     document.body.appendChild(div);
 
-    var mapInit = L.map(this.$map).setView([50.641111, 4.668056], 1);
+    var map = L.map('map').setView([50.641111, 4.668056], 1);
     L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=tLle2wcpHPrfuS2ObIb7',{
     tileSize: 512,
     zoomOffset: -1,
     minZoom: 5,
     attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
     crossOrigin: true
-    }).addTo(mapInit);
+    }).addTo(map);
         
     addMarkerToMap(50.641111,4.668056,"BE",map);
     addMarkerToMap(51,10,"DE",map);
