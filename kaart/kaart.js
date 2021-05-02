8d2be5f5d129718a969e42588bb972eeb8d29396
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-05-02 16:10:41 +0200

    Update kaart.js

--- kaart/kaart.js
+++ kaart/kaart.js
@@ -90,7 +90,7 @@
 
             this.addEventListener("click", () => {
                 var properties = {selectedRegionISO2 : selectedLocation};
-                this.dispatchEvent(new Event("onSelect", 
+                this.dispatchEvent(new Event("onSelect"), 
                 {
                     detail: 
                     {
