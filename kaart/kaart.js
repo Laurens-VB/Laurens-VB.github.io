2f0b9553faad74d7a4cd529e36e123f49002de2a
Laurens-VB <67965463+Laurens-VB@users.noreply.github.com>
2021-05-03 17:53:15 +0200

    Update kaart.js

--- kaart/kaart.js
+++ kaart/kaart.js
@@ -24,7 +24,7 @@
 		constructor() 
         {
             super();
-            console.log("me neme is jeff");
+            console.log("me neme is jeff 2");
             this._firstConnection = true;
 
 			this._shadowRoot = this.attachShadow({mode: "open"});
